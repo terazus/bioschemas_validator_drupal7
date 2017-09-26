282f2ce76bf5e8f8cfdab128c2e7a1afe4a221db
Terazus <batistadominique@hotmail.com>
2017-09-26 23:55:41 +0200

    Json to right panel

--- extra/Bioschemas_builder/app.js
+++ extra/Bioschemas_builder/app.js
@@ -1,11 +1,29 @@
 (function () {
 	var app = angular.module('builder', []);
 
-	app.controller('BuilderController', function(){
-		this.items = specs;
+	app.controller('PanelController', function(){
+		this.tab = -1;
+		this.subTab = false;
+		this.subValue = null;
+		this.current_object = null;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+			this.subTab = false;
+			this.subValue = null;
+			this.current_object = null;
+		}
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		}
+
+		this.selectSubTab = function(){
+			this.subTab = true;
+		}
 	});
 
-	app.controller('CreateController', ['$scope', '$log', function($scope, $log){
+	app.controller('CreateController', function(){
 		this.specs = specs;
 		this.values = make_spec(specs);
 
@@ -77,36 +95,80 @@
 			};
 			return values;
 		}
+	});
 
-	}]);
+	app.controller('OutputController', function(){
+		this.json = {};
+
+		this.initiate = function(object, specs){
+			this.json = {};
+			this.json["@type"] = object.alias;
+			for (key in specs){
+				if (specs[key].name == object.name){
+					for (subkey in specs[key].fields){
+						var field = specs[key].fields[subkey];
+						if (field.state == 'required'){
+							this.json[field.field_name] = "null";
+						}
+					}
+				}
+			}
+		}
+
+		this.addItem = function(){
+			console.log('Form was validated');
+		}
+	});
 
 
 	var specs = [
-	{
-		name : 'Tool',
-		alias : 'SoftwareApplication',
-		spec_path : 'specs/default/softwareapplication/specifications.json',
-	},
-	{
-		name : 'Event',
-		alias : 'Event',
-		spec_path : 'specs/default/event/specifications.json',
-	}
+		{
+			name : 'Tool',
+			alias : 'SoftwareApplication',
+			spec_path : 'specs/default/softwareapplication/specifications.json',
+		},
+		{
+			name : 'Event',
+			alias : 'Event',
+			spec_path : 'specs/default/event/specifications.json',
+		},
+		{
+			name : 'Training Material',
+			alias : 'CreativeWork',
+			spec_path : 'specs/default/creativework/specifications.json',
+		}
 	];
 
+
+	app.directive('outputCode', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'include/output-snippet.html'
+		};
+	});
+
+	app.directive('fieldEditor', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'include/field-editor.html'
+		};
+	});
+
+	app.directive('fieldSelecttor', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'include/field-selector.html'
+		};
+	});
+
+	app.directive('objectSelector', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'include/object-selector.html'
+		};
+	});
 
 })();
 
 
 
-(function($)
-	{
-	  	$(document).ready(function()
-	  	{
-	  		$( ".addfield_button" ).click(function()
-		  	{
-		  		$('.field_values').show();
-			});
-		});
-	}
-)(jQuery);
